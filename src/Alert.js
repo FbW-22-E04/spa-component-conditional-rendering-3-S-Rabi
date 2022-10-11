0fa8df6326070d6cbbec24df928e66da7da754a3
S Rabi <rabi.sanaye@yahoo.com>
2022-10-11 16:15:47 +0200

    The fifth ex

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,13 @@
-import React from "react"
-const Alert = props => {
-	//add the condition inside this function
-
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+import React from "react";
+const Alert = (props) => {
+  //add the condition inside this function
+  if (props.show === false) {
+    return null;
+  } else if (props.show === true)
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is a primary alert-check it out!
+      </div>
+    );
 };
-
 export default Alert;
